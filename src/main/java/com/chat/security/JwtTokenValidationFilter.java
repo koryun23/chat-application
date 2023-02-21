60c695705259f127971df9b32862847871af739e
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2023-02-21 19:36:42 +0400

    adding the security configuration and implementing the jwt token validation filter

--- src/main/java/com/chat/security/JwtTokenValidationFilter.java
+++ src/main/java/com/chat/security/JwtTokenValidationFilter.java
@@ -1,5 +1,10 @@
 package com.chat.security;
 
+import com.chat.service.core.jwt.JwtService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,11 +12,53 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Component
 public class JwtTokenValidationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtTokenValidationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        if (invalidToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        removeTokenPrefix(token);
+        if (jwtService.isExpired(token)) {
+            response.setStatus(401);
+            return;
+        }
+        String username = jwtService.getUsername(token);
+        Set<SimpleGrantedAuthority> simpleGrantedAuthoritySet = jwtService.getAuthorities(token)
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+        if (username != null) {
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    username, null, simpleGrantedAuthoritySet
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            SecurityContextHolder.getContext().setAuthentication(null);
+        }
 
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean invalidToken(String token) {
+        return token == null || !token.startsWith("Bearer ");
+    }
+
+    private void removeTokenPrefix(String token) {
+        token.substring(7);
     }
 }
--- src/main/java/com/chat/security/SecurityConfig.java
+++ src/main/java/com/chat/security/SecurityConfig.java
@@ -1,7 +1,73 @@
 package com.chat.security;
 
+import com.chat.service.core.jwt.JwtService;
+import com.chat.service.core.user.UserAppRoleService;
+import com.chat.service.core.user.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final UserAppRoleService userAppRoleService;
+    private final JwtTokenValidationFilter jwtTokenValidationFilter;
+
+    public SecurityConfig(JwtService jwtService, UserDetailsService userDetailsService, PasswordEncoder passwordEncoder, UserService userService, UserAppRoleService userAppRoleService, JwtTokenValidationFilter jwtTokenValidationFilter) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+        this.userAppRoleService = userAppRoleService;
+        this.jwtTokenValidationFilter = jwtTokenValidationFilter;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(jwtTokenValidationFilter)
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedMethods(List.of(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()
+        ));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
 }
